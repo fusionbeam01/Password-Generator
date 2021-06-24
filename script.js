35f9afd9d3955604bdd4114ec71628794d40c552
Kyle Mularoni <kylemularoni@gmail.com>
2021-06-24 11:59:55 -0400

    edits

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lower = ["a","b","c","d","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upper = ["A","B","C","D","E","F","F","G","H","I","J","K","l","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var num = ["0","1","2","3","4","5","6","7","8","9"];
+var special = ["!","@","#","$","%","^","&",];
+// Use math.random with the lenght of an aray
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
